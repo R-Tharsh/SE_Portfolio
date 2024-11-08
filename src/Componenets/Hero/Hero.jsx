36847673930a802d43347ac060600553f1057835
robert_tharshan <it20075880@my.sliit.lk>
2024-11-08 19:17:27 +0530

    responsive_update

--- src/Componenets/Hero/Hero.jsx
+++ src/Componenets/Hero/Hero.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_image.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
+
 const Hero = () => {
   return (
-    <div id="home" className="hero">
-      <img src={profile_img} alt="" />
-      <h1>
+    <section id="home" className="hero">
+      <img src={profile_img} alt="Profile" className="hero-image" />
+      <h1 className="hero-heading">
         <span>I'm Robert Anandaraj Tharshan,</span> Software Engineer based in
         Sri Lanka.
       </h1>
-      <p>
+      <p className="hero-description">
         As a fervent and aspiring Software Engineer, I'm geared up to embark on
         my journey within the software development domain. Equipped with a
         robust educational background in BSc (Hons) in Information Technology,
@@ -18,14 +19,13 @@ const Hero = () => {
       </p>
       <div className="hero-action">
         <div className="hero-connect">
-          {" "}
           <AnchorLink className="anchor-link" offset={50} href="#contact">
             Connect with me
           </AnchorLink>
         </div>
         <div className="hero-resume">My resume</div>
       </div>
-    </div>
+    </section>
   );
 };
 
