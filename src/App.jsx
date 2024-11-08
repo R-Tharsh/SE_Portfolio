cb6d0696d83bd58ce49d9a5eeaacc42e41b87ea9
robert_tharshan <it20075880@my.sliit.lk>
2024-11-08 14:35:19 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import About from "./Componenets/About/About";
 import Services from "./Componenets/Services/Services";
 import MyWork from "./Componenets/MyWork/MyWork";
 import Contact from "./Componenets/Contact/Contact";
+import Footer from "./Componenets/Footer/Footer";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
       <Services />
       <MyWork />
       <Contact />
+      <Footer />
     </div>
   );
 };
--- src/Componenets/Hero/Hero.jsx
+++ src/Componenets/Hero/Hero.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Hero.css";
 import profile_img from "../../assets/profile_image.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 const Hero = () => {
   return (
-    <div className="hero">
+    <div id="home" className="hero">
       <img src={profile_img} alt="" />
       <h1>
-        <span>I'm Robert Anandaraj Tharshan,</span> Web developer based in Sri
-        Lanka.
+        <span>I'm Robert Anandaraj Tharshan,</span> Software Engineer based in
+        Sri Lanka.
       </h1>
       <p>
         As a fervent and aspiring Software Engineer, I'm geared up to embark on
@@ -16,7 +17,12 @@ const Hero = () => {
         specializing in Software Engineering.
       </p>
       <div className="hero-action">
-        <div className="hero-connect">Connect with me</div>
+        <div className="hero-connect">
+          {" "}
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me
+          </AnchorLink>
+        </div>
         <div className="hero-resume">My resume</div>
       </div>
     </div>
