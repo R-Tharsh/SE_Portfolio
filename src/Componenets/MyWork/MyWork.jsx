91a75c7997d7e1217dd03bcb6a78e6f9803916f5
robert_tharshan <it20075880@my.sliit.lk>
2024-11-13 08:21:59 +0530

    experience_update

--- src/Componenets/MyWork/MyWork.jsx
+++ src/Componenets/MyWork/MyWork.jsx
@@ -7,31 +7,31 @@ const MyWork = () => {
       name: "JobArmer",
       description:
         " JobArmer is the ultimate solution designed to streamline and simplify the way service contractors to manage field workforce. ",
-      link: "https://github.com/yourusername/task-tracker",
+      link: "https://jobarmer.com/",
     },
     {
       name: "AI Powered Travel Recommendation System",
       description:
         "  This research project proposes the development of a personalized travel recommendation system that caters to individual traveler's unique needs and preferences.",
-      link: "https://github.com/yourusername/portfolio",
+      link: "https://github.com/R-Tharsh",
     },
     {
       name: "Thalsevena Beach Resort Management System",
       description:
         " This is a full web application. We used Java, SERVELETS ,JSP,  HTML, CSS, Bootstrap and MySQL for develop this system & our project contains eight sub-systems",
-      link: "https://github.com/yourusername/portfolio",
+      link: "https://github.com/R-Tharsh/Beach-Resort-Management-System---Y2-S2-",
     },
     {
       name: "Online Laundry Management System",
       description:
         "This is a full web application. We used React, Express js, Node js and MongoDB for develop this web application. Our project contains user site, admin site, manager site, deliverer site and worker site.",
-      link: "https://github.com/yourusername/student-management",
+      link: "https://github.com/bSathursan/Washapp-SPM-SER043",
     },
     {
       name: "Portfolio Website",
       description:
         "A personal portfolio built with React and Vite. Showcasing my web development skills and projects.",
-      link: "https://github.com/yourusername/student-management",
+      link: "https://github.com/R-Tharsh/SE_Portfolio",
     },
   ];
 
